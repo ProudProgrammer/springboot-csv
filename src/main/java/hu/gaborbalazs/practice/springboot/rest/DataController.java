0bc2ce654a2e097ba9bb6a7c3a1409c72f6c475b
Gábor Balázs <gbazsi82@gmail.com>
2020-06-29 14:04:12 +0200

    gitignore

--- src/main/java/hu/gaborbalazs/practice/springboot/rest/DataController.java
+++ src/main/java/hu/gaborbalazs/practice/springboot/rest/DataController.java
@@ -1,34 +1,33 @@
 package hu.gaborbalazs.practice.springboot.rest;
 
-import java.io.IOException;
-import java.util.List;
-
+import hu.gaborbalazs.practice.springboot.data.DataStoreProcessor;
+import hu.gaborbalazs.practice.springboot.exception.BaseException;
+import hu.gaborbalazs.practice.springboot.exception.ExceptionType;
+import hu.gaborbalazs.practice.springboot.model.Data;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import hu.gaborbalazs.practice.springboot.data.DataStoreProcessor;
-import hu.gaborbalazs.practice.springboot.exception.BaseException;
-import hu.gaborbalazs.practice.springboot.exception.ExceptionType;
-import hu.gaborbalazs.practice.springboot.model.Data;
+import java.io.IOException;
+import java.util.List;
 
 @RestController
 public class DataController {
 
-	@Autowired
-	private Logger logger;
+    @Autowired
+    private Logger logger;
 
-	@Autowired
-	private DataStoreProcessor dataStoreProcessor;
+    @Autowired
+    private DataStoreProcessor dataStoreProcessor;
 
-	@RequestMapping("data")
-	public List<Data> getDataStore() {
-		try {
-			return dataStoreProcessor.getAllDataFromDataStores();
-		} catch (IOException e) {
-			logger.error(ExceptionType.DATA_STORE_CORRUPT.getExceptionMessage(), e);
-			throw new BaseException(ExceptionType.DATA_STORE_CORRUPT);
-		}
-	}
+    @RequestMapping("data")
+    public List<Data> getDataStore() {
+        try {
+            return dataStoreProcessor.getAllDataFromDataStores();
+        } catch (IOException e) {
+            logger.error(ExceptionType.DATA_STORE_CORRUPT.getExceptionMessage(), e);
+            throw new BaseException(ExceptionType.DATA_STORE_CORRUPT);
+        }
+    }
 }
